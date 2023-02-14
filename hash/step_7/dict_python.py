7c5b6e1885f95079b3c3a1754042602922d8ceb7
ne_andrey <sorokinac@gmail.com>
2023-02-14 13:24:55 +0500

    hash/step_7

--- hash/step_7/dict_python.py
+++ hash/step_7/dict_python.py
@@ -1,6 +1,6 @@
 d = {'one': 1, 'two': 2, 'natural': 1, 'True': 1, 'even': 2, 'three': 3, 'False': 0}  # этот словарь не менять
 
 # здесь продолжайте программу
-d_unique = {v: k for k, v in {v : k for k, v in d.items()}.items()}
+d_unique = {v: k for k, v in {v: k for k, v in d.items()}.items()}
 
 print(d_unique)
